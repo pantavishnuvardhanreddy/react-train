72c0b3ee4de5acd607d09b679efc8158de2344fc
unknown <cogjava1782@iiht.tech>
2022-07-05 05:26:28 -0700

    first commit

---
+++ src/orderform.js
@@ -0,0 +1,9 @@
+function OrderForm(props){
+    return(
+        <div>
+            <h1>Order form</h1>
+            <input>value={props.name}</input>
+        </div>
+    )
+}
+export default OrderForm;
