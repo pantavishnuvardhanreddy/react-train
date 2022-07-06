75afdf8335080bbf85c13c5af2f64c1744b63879
unknown <cogjava1782@iiht.tech>
2022-07-06 05:40:54 -0700

    firstcommmit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 
 import './App.css';
-import OrderForm  from './orderform';
+import OrderForm from './orderform';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+       <OrderForm name = "vishnu"></OrderForm>
+       <OrderForm name = "suresh"></OrderForm>
       </header>
-      <OrderForm name = "vishnu"></OrderForm>
-      <OrderForm name = "suresh"></OrderForm>
     </div>
   );
 }
--- src/orderform.js
+++ src/orderform.js
@@ -1,8 +1,34 @@
-function OrderForm(props){
-    return(
+import { useState } from "react"
+
+
+
+function OrderForm(props) {
+    const [order,setOrder]= useState({
+        item:'laptop',
+        price:33987,
+        tax:10
+    });
+    function updateValue(event){
+        console.log(event.target.value);
+        setOrder({...order, [event.target.name]:event.target.value});
+    }
+    function calculateTotal(){
+        if (order.tax < 0){
+            return "error";
+        }
+        return parseInt(order.price) * (1+ (order.tax / 100));
+    }
+    return (
         <div>
-            <h1>Order form</h1>
-            <input>value={props.name}</input>
+            <h1>order form</h1>
+            <input value={order.item} name='item' onChange={updateValue}></input>
+            <input value={order.price} name='price' onChange={updateValue}></input>
+            <input placeholder='tax' min='0' type='number'  value={order.tax} onChange={updateValue} name='tax'></input>
+            Total: {calculateTotal()}
+            <div>
+                <h1>{props.name}</h1>
+                <h1>{order.item}</h1>
+            </div>
         </div>
     )
 }
